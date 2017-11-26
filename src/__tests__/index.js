432b8fa449a86d575bdaa50c6f18f2de2d1f3dd0
Luca Nils Schmid <me@lucaschmid.net>
2017-11-26 16:57:41 +0100

    feat(index.js): implement some useful functions

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,16 +1,40 @@
 // @flow
 
-const { restore } = require('../')
+const { setup, restore, runGuarded, guard } = require('../')
 const mobx = require('mobx')
 
 describe('BaseAtom.reportObserved', () => {
   it('should throw if a derivation is untracked', () => {
+    setup()
     const observable = mobx.observable({ x: true })
+
     expect(() => { observable.x }).toThrow()
+
+    restore()
+  })
+
+  it('should allow untracked access to mobx-react props', () => {
+    const atom = new mobx.Atom('reactive props', () => {}, () => {})
+    const fn = guard(() => { atom.reportObserved })
+    expect(fn).not.toThrow()
+  })
+
+  it('should allow untracked access to mobx-react state', () => {
+    const atom = new mobx.Atom('reactive state', () => {}, () => {})
+    const fn = guard(() => { atom.reportObserved })
+    expect(fn).not.toThrow()
+  })
+
+  it('should allow untracked access withing actions', () => {
+    const x = mobx.observable({ y: true })
+    const action = guard(() => mobx.runInAction(() => x.y))
+    expect(action).not.toThrow()
   })
 })
 
 describe('mobx.untracked', () => {
+  setup()
+
   it('should allow running untracked derivations', () => {
     const observable = mobx.observable({ x: true })
     const fn = () => mobx.untracked(() => { observable.x })
@@ -25,6 +49,8 @@ describe('mobx.untracked', () => {
 
     expect(output).toBe(returnValue)
   })
+
+  restore()
 })
 
 describe('restore', () => {
@@ -32,5 +58,44 @@ describe('restore', () => {
     restore()
     const observable = mobx.observable({ x: true })
     expect(() => { observable.x }).not.toThrow()
+  })
+})
+
+describe('runGuarded', () => {
+  restore()
+  const x = mobx.observable({ y: true })
+
+  it('should disallow untracked derivations', () => {
+    const fn = () => runGuarded(() => { x.y })
+    expect(fn).toThrow()
+  })
+
+  it('should propagate the return value', () => {
+    const expected = Symbol('Test')
+    const output = runGuarded(() => expected)
+    expect(output).toBe(expected)
+  })
+})
+
+describe('guard', () => {
+  it('should call the given function in a guard', () => {
+    const x = mobx.observable({ y: true })
+    const fn = guard(() => { x.y })
+    expect(fn).toThrow()
+  })
+
+  it('should pass the given arguments to the function', () => {
+    const fn = jest.fn()
+    const args = [1, 2, 3]
+
+    guard(fn)(...args)
+
+    expect(fn).toHaveBeenCalledWith(...args)
+  })
+
+  it('should propagate the return value', () => {
+    const expected = Symbol('Test')
+    const output = guard(() => expected)()
+    expect(output).toBe(expected)
   })
 })
--- src/index.js
+++ src/index.js
@@ -4,30 +4,65 @@ const mobx = require('mobx')
 
 let isExplicitUntract = false
 
+const untracked = mobx.untracked
 const reportObserved = mobx.BaseAtom.prototype.reportObserved
 
-mobx.BaseAtom.prototype.reportObserved = function () {
+const guardedReportObserved = function () {
   if (
+    this.name !== 'reactive props' &&
+    this.name !== 'reactive state' &&
+    mobx.extras.getGlobalState().inBatch === 0 &&
     !isExplicitUntract &&
     !mobx.extras.isComputingDerivation()
   ) {
+    //console.log(this)
     throw new Error('Untracked access to atom')
   }
 
   reportObserved.call(this)
 }
 
-const untracked = mobx.untracked
-const untrackedOverride = <T>(fn: () => T): T => {
+const guardedUntracked = <T>(fn: () => T): T => {
   isExplicitUntract = true
   const returnValue = untracked(fn)
   isExplicitUntract = false
   return returnValue
 }
 
-mobx.untracked = untrackedOverride
+const setup = module.exports.setup = (): void => {
+  mobx.BaseAtom.prototype.reportObserved = guardedReportObserved
 
-module.exports.restore = (): void => {
+  Reflect.defineProperty(mobx, 'untracked', {
+    value: guardedUntracked,
+    configurable: true,
+  })
+
+  Reflect.defineProperty(mobx.default, 'untracked', {
+    value: guardedUntracked,
+    configurable: true,
+  })
+}
+
+const restore = module.exports.restore = (): void => {
   mobx.untracked = untracked
   mobx.BaseAtom.prototype.reportObserved = reportObserved
 }
+
+const runGuarded = module.exports.runGuarded = <T>(fn: () => T): T => {
+  setup()
+  const returnValue = fn()
+  restore()
+  return returnValue
+}
+
+type Guard =
+  <T, A>(fn: (A) => T) => (a: A) => T
+
+const guard = <T, A>(
+    fn: (...args: Array<A>) => T
+  ): (...args: Array<A>) => T =>
+    (...args: Array<A>): T =>
+      runGuarded(() => fn(...args))
+
+module.exports.guard = guard
+module.exports.untracked = guardedUntracked
